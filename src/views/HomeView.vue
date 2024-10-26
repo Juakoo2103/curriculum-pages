dbdf6e7c9eddc7b18bd8233c3333faada23b7988
Juakoo2103 <joaquineduardo.gonzalez@gmail.com>
2024-10-25 21:29:01 -0300

    cambios a luis

---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Pagina principal de los curriculums</h1>
+</template>
+
+<script>
+export default {
+  name: "HomeView",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped></style>
